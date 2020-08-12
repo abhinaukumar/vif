6677877055931331d6ba0a4ebe82276a795df79e
Abhinau Kumar <ab.kumr98@gmail.com>
2020-08-12 14:58:27 -0500

    Initial commit

---
+++ tests/python_live_vqa.py
@@ -0,0 +1,97 @@
+import numpy as np
+import cv2
+# from utils import structure_sim
+from ..vif_utils import vif
+
+from scipy.io import loadmat, savemat
+from scipy.stats import spearmanr, pearsonr
+from scipy.optimize import curve_fit
+
+import pandas as pd
+import os
+import argparse
+
+import progressbar
+
+parser = argparse.ArgumentParser(description="Code to generate SSIM data for LIVE IQA Database")
+parser.add_argument("--path", help="Path to database", required=True)
+args = parser.parse_args()
+
+f = loadmat('data/live_vqa_scores.mat')
+scores = f['scores'].squeeze()
+scores = (scores - np.min(scores))/(np.max(scores) - np.min(scores))
+
+df = pd.read_csv(os.path.join(args.path, 'live_video_quality_seqs.txt'), header=None, engine='python')
+file_list = df.values[:, 0]
+
+refs = ["pa", "rb", "rh", "tr", "st", "sf", "bs", "sh", "mc", "pr"]
+fps = [25, 25, 25, 25, 25, 25, 25, 50, 50, 50]
+fps = [str(f) + 'fps' for f in fps]
+
+n_refs = len(refs)
+
+widgets = [
+            progressbar.ETA(),
+            progressbar.Bar(),
+            ' ', progressbar.DynamicMessage('file'),
+            '/', progressbar.DynamicMessage('total')
+            ]
+
+sims = np.zeros((n_refs*15,))
+sim_spat_alls = np.empty((n_refs*15,), dtype=object)
+sim_temp_alls = np.empty((n_refs*15,), dtype=object)
+k = 0
+with progressbar.ProgressBar(max_value=n_refs*15, widgets=widgets) as bar:
+    for i_ref, ref in enumerate(refs):
+        v_ref = cv2.VideoCapture(os.path.join(args.path, 'videos', ref + '_Folder', 'rgb', ref + '1' + '_' + fps[i_ref] + '.mp4'))
+        w = int(v_ref.get(cv2.CAP_PROP_FRAME_WIDTH))
+        h = int(v_ref.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        for i_dist in range(2, 17):
+            v_dist = cv2.VideoCapture(os.path.join(args.path, 'videos', ref + '_Folder', 'rgb', ref + str(i_dist) + '_' + fps[i_ref] + '.mp4'))
+            if k < 103:
+                k += 1
+                continue
+            y_ref_prev = np.zeros((h, w))
+            y_dist_prev = np.zeros((h, w))
+            spat_scores = []
+            temp_scores = []
+            while(v_ref.isOpened() and v_dist.isOpened()):
+                ret_ref, rgb_ref = v_ref.read()
+                ret_dist, rgb_dist = v_dist.read()
+
+                if ret_ref and ret_dist:
+                    y_ref = cv2.cvtColor(rgb_ref, cv2.COLOR_BGR2YUV)[:, :, 0].astype('float32')
+                    y_dist = cv2.cvtColor(rgb_dist, cv2.COLOR_BGR2YUV)[:, :, 0].astype('float32')
+
+                    # temp.append(structure_sim(y_ref, y_dist, 2, 2))
+                    spat_scores.append(vif(y_ref, y_dist))
+                    temp_scores.append(vif(y_ref - y_ref_prev, y_dist - y_dist_prev))
+                    y_ref_prev = y_ref
+                    y_dist_prev = y_dist
+                else:
+                    break
+
+            sims[k] = np.mean(spat_scores)*np.mean(temp_scores[1:])
+            sim_spat_alls[k] = np.array(spat_scores)
+            sim_temp_alls[k] = np.array(temp_scores[1:])
+            k += 1
+            v_ref.set(cv2.CAP_PROP_POS_FRAMES, 0)
+            bar.update(i_ref*15 + i_dist - 2, file=i_ref*15+i_dist-1, total=n_refs*15)
+
+savemat('data/live_vqa_stvifs.mat', {'spat_vifs': sim_spat_alls, 'temp_vifs': sim_temp_alls})
+
+# Fitting logistic function to SSIM
+[[b0, b1, b2, b3, b4], _] = curve_fit(lambda t, b0, b1, b2, b3, b4: b0 * (0.5 - 1.0/(1 + np.exp(b1*(t - b2))) + b3 * t + b4),
+                                      sims, scores, p0=0.5*np.ones((5,)), maxfev=20000)
+
+scores_pred = b0 * (0.5 - 1.0/(1 + np.exp(b1*(sims - b2))) + b3 * sims + b4)
+
+pcc = pearsonr(scores_pred, scores)[0]
+srocc = spearmanr(scores_pred, scores)[0]
+rmse = np.sqrt(np.mean((scores_pred - scores)**2))
+
+print("PCC:", pcc)
+print("SROCC:", srocc)
+print("RMSE:", rmse)
+
+savemat('data/live_vqa_stvifs.mat', {'spat_vifs': sim_spat_alls, 'temp_vifs': sim_temp_alls, 'pcc': pcc, 'srocc': srocc, 'rmse': rmse})
---
+++ vif_utils.py
@@ -0,0 +1,165 @@
+from pyrtools.pyramids import SteerablePyramidSpace as SPyr
+import numpy as np
+
+
+def im2col(img, k, stride=1):
+    # Parameters
+    m, n = img.shape
+    s0, s1 = img.strides
+    nrows = m - k + 1
+    ncols = n - k + 1
+    shape = (k, k, nrows, ncols)
+    arr_stride = (s0, s1, s0, s1)
+
+    ret = np.lib.stride_tricks.as_strided(img, shape=shape, strides=arr_stride)
+    return ret[:, :, ::stride, ::stride].reshape(k*k, -1)
+
+
+def integral_image(x):
+    M, N = x.shape
+    int_x = np.zeros((M+1, N+1))
+    int_x[1:, 1:] = np.cumsum(np.cumsum(x, 0), 1)
+    return int_x
+
+
+def moments(x, y, k, stride):
+    kh = kw = k
+
+    k_norm = k**2
+
+    x_pad = np.pad(x, int((kh - stride)/2), mode='reflect')
+    y_pad = np.pad(y, int((kh - stride)/2), mode='reflect')
+
+    int_1_x = integral_image(x_pad)
+    int_1_y = integral_image(y_pad)
+
+    int_2_x = integral_image(x_pad**2)
+    int_2_y = integral_image(y_pad**2)
+
+    int_xy = integral_image(x_pad*y_pad)
+
+    mu_x = (int_1_x[:-kh:stride, :-kw:stride] - int_1_x[:-kh:stride, kw::stride] - int_1_x[kh::stride, :-kw:stride] + int_1_x[kh::stride, kw::stride]) / k_norm
+    mu_y = (int_1_y[:-kh:stride, :-kw:stride] - int_1_y[:-kh:stride, kw::stride] - int_1_y[kh::stride, :-kw:stride] + int_1_y[kh::stride, kw::stride]) / k_norm
+
+    var_x = np.clip((int_2_x[:-kh:stride, :-kw:stride] - int_2_x[:-kh:stride, kw::stride] - int_2_x[kh::stride, :-kw:stride] + int_2_x[kh::stride, kw::stride]) / k_norm - mu_x**2, 0, None)
+    var_y = np.clip((int_2_y[:-kh:stride, :-kw:stride] - int_2_y[:-kh:stride, kw::stride] - int_2_y[kh::stride, :-kw:stride] + int_2_y[kh::stride, kw::stride]) / k_norm - mu_y**2, 0, None)
+
+    cov_xy = (int_xy[:-kh:stride, :-kw:stride] - int_xy[:-kh:stride, kw::stride] - int_xy[kh::stride, :-kw:stride] + int_xy[kh::stride, kw::stride]) / k_norm - mu_x*mu_y
+
+    return (mu_x, mu_y, var_x, var_y, cov_xy)
+
+
+def vif_gsm_model(pyr, subband_keys, M):
+    tol = 1e-15
+    s_all = []
+    lamda_all = []
+
+    for subband_key in subband_keys:
+        y = pyr[subband_key]
+        y_size = (int(y.shape[0]/M)*M, int(y.shape[1]/M)*M)
+        y = y[:y_size[0], :y_size[1]]
+
+        y_vecs = im2col(y, M, 1)
+        cov = np.cov(y_vecs)
+        lamda, V = np.linalg.eigh(cov)
+        lamda[lamda < tol] = tol
+        cov = V@np.diag(lamda)@V.T
+
+        y_vecs = im2col(y, M, M)
+
+        s = np.linalg.inv(cov)@y_vecs
+        s = np.sum(s * y_vecs, 0)/(M*M)
+        s = s.reshape((int(y_size[0]/M), int(y_size[1]/M)))
+
+        s_all.append(s)
+        lamda_all.append(lamda)
+
+    return s_all, lamda_all
+
+
+def vif_channel_est(pyr_ref, pyr_dist, subband_keys, M):
+    tol = 1e-15
+    g_all = []
+    sigma_vsq_all = []
+
+    for i, subband_key in enumerate(subband_keys):
+        y_ref = pyr_ref[subband_key]
+        y_dist = pyr_dist[subband_key]
+
+        lev = int(np.ceil((i+1)/2))
+        winsize = 2**lev + 1
+
+        y_size = (int(y_ref.shape[0]/M)*M, int(y_ref.shape[1]/M)*M)
+        y_ref = y_ref[:y_size[0], :y_size[1]]
+        y_dist = y_dist[:y_size[0], :y_size[1]]
+
+        mu_x, mu_y, var_x, var_y, cov_xy = moments(y_ref, y_dist, winsize, M)
+
+        g = cov_xy / (var_x + tol)
+        sigma_vsq = var_y - g*cov_xy
+
+        g[var_x < tol] = 0
+        sigma_vsq[var_x < tol] = var_y[var_x < tol]
+        var_x[var_x < tol] = 0
+
+        g[var_y < tol] = 0
+        sigma_vsq[var_y < tol] = 0
+
+        sigma_vsq[g < 0] = var_y[g < 0]
+        g[g < 0] = 0
+
+        sigma_vsq[sigma_vsq < tol] = tol
+
+        g_all.append(g)
+        sigma_vsq_all.append(sigma_vsq)
+
+    return g_all, sigma_vsq_all
+
+
+def vif(img_ref, img_dist):
+    M = 3
+    sigma_nsq = 0.1
+
+    pyr_ref = SPyr(img_ref, 4, 5, 'reflect1').pyr_coeffs
+    pyr_dist = SPyr(img_dist, 4, 5, 'reflect1').pyr_coeffs
+
+    subband_keys = []
+    for key in list(pyr_ref.keys())[1:-2:3]:
+        subband_keys.append(key)
+    subband_keys.reverse()
+    n_subbands = len(subband_keys)
+
+    [g_all, sigma_vsq_all] = vif_channel_est(pyr_ref, pyr_dist, subband_keys, M)
+
+    [s_all, lamda_all] = vif_gsm_model(pyr_ref, subband_keys, M)
+
+    nums = np.zeros((n_subbands,))
+    dens = np.zeros((n_subbands,))
+    for i in range(n_subbands):
+        g = g_all[i]
+        sigma_vsq = sigma_vsq_all[i]
+        s = s_all[i]
+        lamda = lamda_all[i]
+
+        n_eigs = len(lamda)
+
+        lev = int(np.ceil((i+1)/2))
+        winsize = 2**lev + 1
+        offset = (winsize - 1)/2
+        offset = int(np.ceil(offset/M))
+
+        g = g[offset:-offset, offset:-offset]
+        sigma_vsq = sigma_vsq[offset:-offset, offset:-offset]
+        s = s[offset:-offset, offset:-offset]
+
+        temp = 0
+        dens[i] = 1
+        for j in range(n_eigs):
+            # nums[i] += np.sum(np.log(1 + g*g*s*lamda[j]/(sigma_vsq+sigma_nsq)))
+            # dens[i] += np.sum(np.log(1 + s*lamda[j]/sigma_nsq))
+            # temp = temp + np.abs(np.log(g*g*s*lamda[j] + sigma_vsq + sigma_nsq) - np.log(s*lamda[j] + sigma_nsq))
+            temp = temp + np.sum(np.log(1 + g*g*s*lamda[j]/sigma_vsq))
+        nums[i] = np.mean(temp)
+
+    return np.sum(nums)/np.sum(dens)
+    # return np.mean(nums/dens)
